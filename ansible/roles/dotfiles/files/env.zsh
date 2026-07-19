317fc4f41ccd4e8c483533fab8e647b5b517572d
Mirco Santosuosso <mirco.santosuosso@gmail.com>
2026-07-19 18:47:01 +0200

    Add NVM and Golang environment variables to env.zsh

--- ansible/roles/dotfiles/files/env.zsh
+++ ansible/roles/dotfiles/files/env.zsh
@@ -1,3 +1,10 @@
 TERM="xterm-256color"
+
+# Golang
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:~/go/bin
+
+# NVM / NodeJs
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
